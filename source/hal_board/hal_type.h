9523ca63452a4ead4eb5beda866ae34a634aba99
ZIKEER <zzk11111111@qq.com>
2024-02-20 17:25:45 +0800

    增加待完成文件,修改工程配置

---
+++ source/hal_board/hal_type.h
@@ -0,0 +1,19 @@
+#ifndef HAL_TYPE_H__
+#define HAL_TYPE_H__
+
+#include <stdint.h>
+#include <stddef.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ source/module/common_string.c
@@ -0,0 +1,5 @@
+#include "common_crc.h"
+
+/* todo float64_t -> int32tint32t */
+/* todo float32_t -> int32tint32t */
+
